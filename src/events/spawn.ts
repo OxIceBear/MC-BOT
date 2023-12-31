4b72187013c2b695429af3ba2fe23a4f05864e99
OxIceBear <jovanstankovic010@gmail.com>
2024-01-01 00:37:47 +0100

    Remodified spawn.ts

--- src/events/spawn.ts
+++ src/events/spawn.ts
@@ -6,26 +6,27 @@ import { farming } from "../struct/Farming";
 import inventory from "mineflayer-web-inventory";
 
 const SpawnEvent: Bot.Event = {
-	name: "spawn",
-	once: true,
-	execute: async (manager) => {
-		// reset everything on spawn
-		manager.reset();
+    name: "spawn",
+    once: true,
+    execute: async (manager) => {
+        try {
+            manager.reset();
+            manager.minecraft_data = minecraftData(manager.bot.version);
 
-		manager.minecraft_data = minecraftData(manager.bot.version);
+            await Promise.all([
+                viewer(manager.bot, CONFIG.VIEWER_OPTIONS),
+                inventory(manager.bot, CONFIG.INVENTORY_VIEWER_OPTION),
+            ]);
 
-		viewer(manager.bot, CONFIG.VIEWER_OPTIONS);
-		inventory(manager.bot, CONFIG.INVENTORY_VIEWER_OPTION);
+            create_root_state(manager);
+            manager.fetchChests(manager, manager.minecraft_data);
+            farming(manager);
 
-		create_root_state(manager);
-
-		manager.fetchChests(manager, manager.minecraft_data);
-
-		// physicsTick doesn't works and this one looks kinda messy. Might be changed in the future
-		farming(manager);
-
-		manager.logger.success("Bot successfully spawned");
-	},
+            manager.logger.success("Bot successfully spawned");
+        } catch (error) {
+            manager.logger.error("Error during spawn:", error);
+        }
+    },
 };
 
 export default SpawnEvent;
