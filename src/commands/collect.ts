49570ad40f0799530fa54c86be9a56f38f646178
OxIceBear <jovanstankovic010@gmail.com>
2023-12-31 16:13:52 +0100

    Modified collect a littel
    Bot will now not leave, but instead say there was an error. Im working on a fix for this

--- src/commands/collect.ts
+++ src/commands/collect.ts
@@ -1,69 +1,68 @@
 const CollectCommad: Bot.Command = {
-	name: "collect",
-	aliases: [],
-	args_definitions: [
-		{
-			name: "block",
-			type: String,
-			default: true,
-		},
-		{
-			name: "count",
-			type: Number,
-			aliases: ["c", "n", "number", "limit", "l"],
-		},
-	],
-	master_only: true,
-	execute: async ({ manager, args }) => {
-		if (
-			manager.getCollecting() ||
-			manager.getFarming().farmed_at ||
-			manager.getFalling() ||
-			manager.getGuarding()
-		)
-			return manager.bot.chat(
-				manager.i18n.get(
-					manager.language,
-					"commands",
-					"is_acting",
-				) as string,
-			);
+    name: "collect",
+    aliases: [],
+    args_definitions: [
+        {
+            name: "block",
+            type: String,
+            default: true,
+        },
+        {
+            name: "count",
+            type: Number,
+            aliases: ["c", "n", "number", "limit", "l"],
+        },
+    ],
+    master_only: true,
+    execute: async ({ manager, args }) => {
+        if (
+            manager.getCollecting() ||
+            manager.getFarming().farmed_at ||
+            manager.getFalling() ||
+            manager.getGuarding()
+        )
+            return manager.bot.chat(
+                manager.i18n.get(
+                    manager.language,
+                    "commands",
+                    "is_acting",
+                ) as string,
+            );
 
-		const { block, count } = args;
+        const { block, count } = args;
 
-		if (!block)
-			return manager.bot.chat(
-				manager.i18n.get(
-					manager.language,
-					"commands",
-					"enter_block_name",
-				) as string,
-			);
+        if (!block)
+            return manager.bot.chat(
+                manager.i18n.get(
+                    manager.language,
+                    "commands",
+                    "enter_block_name",
+                ) as string,
+            );
 
-		const block_data =
-			manager.minecraft_data?.blocksByName[block as string];
-		if (!block_data)
-			return manager.bot.chat(
-				manager.i18n.get(
-					manager.language,
-					"commands",
-					"invalid_block",
-				) as string,
-			);
+        const block_data =
+            manager.minecraft_data?.blocksByName[block as string];
+        if (!block_data)
+            return manager.bot.chat(
+                manager.i18n.get(
+                    manager.language,
+                    "commands",
+                    "invalid_block",
+                ) as string,
+            );
 
-		manager
-			.collectBlock(manager, block_data.id, count as number)
-			.then((is_collected) => {
-				if (is_collected)
-					manager.bot.chat(
-						manager.i18n.get(
-							manager.language,
-							"commands",
-							"blocks_collected",
-						) as string,
-					);
-			});
-	},
+        const is_collected = await manager.collectBlock(manager, block_data.id, count as number);
+
+        if (is_collected) {
+            manager.bot.chat(
+                manager.i18n.get(
+                    manager.language,
+                    "commands",
+                    "blocks_collected",
+                ) as string,
+            );
+        }
+    },
 };
 
 export default CollectCommad;
