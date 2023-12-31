1afee82c47df037b40453009dfcc91d8d5e4a216
OxIceBear <jovanstankovic010@gmail.com>
2023-12-31 17:59:59 +0100

    Stupid change!

--- src/events/physicTick.ts
+++ src/events/physicTick.ts
@@ -12,7 +12,7 @@ const PhysicsTickEvent: Bot.Event = {
 			manager.bot.lookAt(entity.position.offset(0, entity.height, 0));
 
 		const mob = manager.bot.nearestEntity(
-			(e) =>
+			(e: { type: string; position: { distanceTo: (arg0: any) => number; }; mobType: string; }) =>
 				e.type === "mob" &&
 				e.position.distanceTo(manager.bot.entity.position) < 16 &&
 				e.mobType !== "Armor Stand",
