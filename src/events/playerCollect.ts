3597869d83c3dab360724ef69beeabdbe3daff87
OxIceBear <jovanstankovic010@gmail.com>
2024-01-01 11:55:48 +0100

    Check for Collector's Existence, Explicit Type for Collector Parameter

--- src/events/playerCollect.ts
+++ src/events/playerCollect.ts
@@ -1,19 +1,19 @@
 import { Entity } from "prismarine-entity";
 
 const PlayerCollectEvent: Bot.Event = {
-	name: "playerCollect",
-	once: false,
-	execute: async (manager, collector: Entity) => {
-		if (!manager.getGuarding() || collector != manager.bot.entity) return;
+    name: "playerCollect",
+    once: false,
+    execute: async (manager, collector: Entity | undefined) => {
+        if (!collector || !manager.getGuarding() || collector !== manager.bot.entity) return;
 
-		const items = manager.bot.inventory.items();
+        const items = manager.bot.inventory.items();
 
-		const sword = items.find((item) => item.name.includes("sword"));
-		if (sword) await manager.bot.equip(sword, "hand");
+        const sword = items.find((item) => item.name.includes("sword"));
+        if (sword) await manager.bot.equip(sword, "hand");
 
-		const shield = items.find((item) => item.name.includes("shield"));
-		if (shield) await manager.bot.equip(shield, "off-hand");
-	},
+        const shield = items.find((item) => item.name.includes("shield"));
+        if (shield) await manager.bot.equip(shield, "off-hand");
+    },
 };
 
 export default PlayerCollectEvent;
