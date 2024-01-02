7580be75c8f095d97bb13431f5d76a2747f3713c
OxIceBear <jovanstankovic010@gmail.com>
2024-01-02 18:58:43 +0100

    Now armor managment works!
    Thanks to MasterObey for helping, he is busy, so he cant help me all of the time!

--- src/events/playerCollect.ts
+++ src/events/playerCollect.ts
@@ -1,18 +1,27 @@
 import { Entity } from "prismarine-entity";
 
+const findItemByName = (items: any[], itemName: string) => items.find((item: { name: string | any[]; }) => item.name.includes(itemName));
+
 const PlayerCollectEvent: Bot.Event = {
     name: "playerCollect",
     once: false,
     execute: async (manager, collector: Entity | undefined) => {
-        if (!collector || !manager.getGuarding() || collector !== manager.bot.entity) return;
+        if (!collector || !manager.getGuarding() || collector !== manager.bot.entity) {
+            return;
+        }
 
         const items = manager.bot.inventory.items();
 
-        const sword = items.find((item) => item.name.includes("sword"));
-        if (sword) await manager.bot.equip(sword, "hand");
+        const sword = findItemByName(items, "sword");
+        const shield = findItemByName(items, "shield");
 
-        const shield = items.find((item) => item.name.includes("shield"));
-        if (shield) await manager.bot.equip(shield, "off-hand");
+        if (sword) {
+            await manager.bot.equip(sword, "hand");
+        }
+
+        if (shield) {
+            await manager.bot.equip(shield, "off-hand");
+        }
     },
 };
 
